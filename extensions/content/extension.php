ba632d3be8e6481c4386c5b97b948149e9652f67
Hogne Titlestad <ht@friendup.cloud>
2024-04-19 08:55:54 +0200

    Fix layout CSS for modern browsers.

--- extensions/content/extension.php
+++ extensions/content/extension.php
@@ -1,4 +1,10 @@
 <?php
 
+global $document;
+
+$document->addResource( 'stylesheet', 'extensions/content/css/extension.css' );
+
+$etpl = new cPTemplate( __DIR__ . '/templates/extension.php' );
+$extension .= $etpl->render();
 
 ?>
